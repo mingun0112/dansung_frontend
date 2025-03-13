071861ccc02fe289d542ba28ea03dea2743fc79d
정민균 <mingun0112@naver.com>
2025-03-13 16:50:47 +0900

    junyong

--- app/page.tsx
+++ app/page.tsx
@@ -69,7 +69,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-[100px] font-bold text-center w-full">DANSUNG</h1>
+        <h1 className="text-[100px] font-bold text-center w-full">권준영의 사이트</h1>
 
         <div className="items-center w-[500px]">
           {!file && (<div>
